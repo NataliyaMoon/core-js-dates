078596f71c8ba8e5f49b546fcd6a9ba68fd2639e
Moon Natalia <nataliya.moon@gmail.com>
2025-03-06 23:55:07 +0500

    feat: fix bug

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -308,9 +308,8 @@ function getWorkSchedule(period, countWorkDays, countOffDays) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(date) {
-  const year = date.getFullYear();
-  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+function isLeapYear(/* date */) {
+  throw new Error('Not implemented');
 }
 
 module.exports = {
